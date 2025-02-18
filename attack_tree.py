be34757f21c44a26dc2bfc8fa212217ed5e06440
gajanandverma <gajanandverma@gmail.com>
2025-02-18 20:52:17 +0530

    new code

---
+++ attack_tree.py
@@ -0,0 +1,179 @@
+import re
+import requests
+import streamlit as st
+from mistralai import Mistral
+from openai import OpenAI, AzureOpenAI
+
+# Function to create a prompt to generate an attack tree
+def create_attack_tree_prompt(app_type, authentication, internet_facing, sensitive_data, app_input):
+    prompt = f"""
+APPLICATION TYPE: {app_type}
+AUTHENTICATION METHODS: {authentication}
+INTERNET FACING: {internet_facing}
+SENSITIVE DATA: {sensitive_data}
+APPLICATION DESCRIPTION: {app_input}
+"""
+    return prompt
+
+
+# Function to get attack tree from the GPT response.
+def get_attack_tree(api_key, model_name, prompt):
+    client = OpenAI(api_key=api_key)
+
+    response = client.chat.completions.create(
+        model=model_name,
+        messages=[
+            {"role": "system", "content": """
+Act as a cyber security expert with more than 20 years experience of using the STRIDE threat modelling methodology to produce comprehensive threat models for a wide range of applications. Your task is to use the application description provided to you to produce an attack tree in Mermaid syntax. The attack tree should reflect the potential threats for the application based on the details given.
+
+You MUST only respond with the Mermaid code block. See below for a simple example of the required format and syntax for your output.
+
+```mermaid
+graph TD
+    A[Enter Chart Definition] --> B(Preview)
+    B --> C{{decide}}
+    C --> D["Keep"]
+    C --> E["Edit Definition (Edit)"]
+    E --> B
+    D --> F["Save Image and Code"]
+    F --> B
+```
+
+IMPORTANT: Round brackets are special characters in Mermaid syntax. If you want to use round brackets inside a node label you MUST wrap the label in double quotes. For example, ["Example Node Label (ENL)"].
+"""},
+            {"role": "user", "content": prompt}
+        ]
+    )
+
+    # Access the 'content' attribute of the 'message' object directly
+    attack_tree_code = response.choices[0].message.content
+    
+    # Remove Markdown code block delimiters using regular expression
+    attack_tree_code = re.sub(r'^```mermaid\s*|\s*```$', '', attack_tree_code, flags=re.MULTILINE)
+
+    return attack_tree_code
+
+# Function to get attack tree from the Azure OpenAI response.
+def get_attack_tree_azure(azure_api_endpoint, azure_api_key, azure_api_version, azure_deployment_name, prompt):
+    client = AzureOpenAI(
+        azure_endpoint = azure_api_endpoint,
+        api_key = azure_api_key,
+        api_version = azure_api_version,
+    )
+
+    response = client.chat.completions.create(
+        model = azure_deployment_name,
+        messages=[
+            {"role": "system", "content": """
+Act as a cyber security expert with more than 20 years experience of using the STRIDE threat modelling methodology to produce comprehensive threat models for a wide range of applications. Your task is to use the application description provided to you to produce an attack tree in Mermaid syntax. The attack tree should reflect the potential threats for the application based on the details given.
+
+You MUST only respond with the Mermaid code block. See below for a simple example of the required format and syntax for your output.
+
+```mermaid
+graph TD
+    A[Enter Chart Definition] --> B(Preview)
+    B --> C{{decide}}
+    C --> D["Keep"]
+    C --> E["Edit Definition (Edit)"]
+    E --> B
+    D --> F["Save Image and Code"]
+    F --> B
+```
+
+IMPORTANT: Round brackets are special characters in Mermaid syntax. If you want to use round brackets inside a node label you MUST wrap the label in double quotes. For example, ["Example Node Label (ENL)"].
+"""},
+            {"role": "user", "content": prompt}
+        ]
+    )
+
+    # Access the 'content' attribute of the 'message' object directly
+    attack_tree_code = response.choices[0].message.content
+    
+    # Remove Markdown code block delimiters using regular expression
+    attack_tree_code = re.sub(r'^```mermaid\s*|\s*```$', '', attack_tree_code, flags=re.MULTILINE)
+
+    return attack_tree_code
+
+# Function to get attack tree from the Mistral model's response.
+def get_attack_tree_mistral(mistral_api_key, mistral_model, prompt):
+    client = Mistral(api_key=mistral_api_key)
+
+    response = client.chat.complete(
+        model=mistral_model,
+        messages=[
+            {"role": "system", "content": """
+Act as a cyber security expert with more than 20 years experience of using the STRIDE threat modelling methodology to produce comprehensive threat models for a wide range of applications. Your task is to use the application description provided to you to produce an attack tree in Mermaid syntax. The attack tree should reflect the potential threats for the application based on the details given.
+
+You MUST only respond with the Mermaid code block. See below for a simple example of the required format and syntax for your output.
+
+```mermaid
+graph TD
+    A[Enter Chart Definition] --> B(Preview)
+    B --> C{{decide}}
+    C --> D["Keep"]
+    C --> E["Edit Definition (Edit)"]
+    E --> B
+    D --> F["Save Image and Code"]
+    F --> B
+```
+
+IMPORTANT: Round brackets are special characters in Mermaid syntax. If you want to use round brackets inside a node label you MUST wrap the label in double quotes. For example, ["Example Node Label (ENL)"].
+"""},
+            {"role": "user", "content": prompt}
+        ]
+    )
+
+    # Access the 'content' attribute of the 'message' object directly
+    attack_tree_code = response.choices[0].message.content
+    
+    # Remove Markdown code block delimiters using regular expression
+    attack_tree_code = re.sub(r'^```mermaid\s*|\s*```$', '', attack_tree_code, flags=re.MULTILINE)
+
+    return attack_tree_code
+
+# Function to get attack tree from Ollama hosted LLM.
+def get_attack_tree_ollama(ollama_model, prompt):
+    
+    url = "http://localhost:11434/api/chat"
+
+    data = {
+        "model": ollama_model,
+        "stream": False,
+        "messages": [
+            {
+                "role": "system", 
+                "content": """
+Act as a cyber security expert with more than 20 years experience of using the STRIDE threat modelling methodology to produce comprehensive threat models for a wide range of applications. Your task is to use the application description provided to you to produce an attack tree in Mermaid syntax. The attack tree should reflect the potential threats for the application based on the details given.
+
+You MUST only respond with the Mermaid code block. See below for a simple example of the required format and syntax for your output.
+
+```mermaid
+graph TD
+    A[Enter Chart Definition] --> B(Preview)
+    B --> C{{decide}}
+    C --> D["Keep"]
+    C --> E["Edit Definition (Edit)"]
+    E --> B
+    D --> F["Save Image and Code"]
+    F --> B
+```
+
+IMPORTANT: Round brackets are special characters in Mermaid syntax. If you want to use round brackets inside a node label you MUST wrap the label in double quotes. For example, ["Example Node Label (ENL)"].
+"""},
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    }
+    response = requests.post(url, json=data)
+
+    outer_json = response.json()
+    
+    # Access the 'content' attribute of the 'message' dictionary
+    attack_tree_code = outer_json["message"]["content"]
+
+    # Remove Markdown code block delimiters using regular expression
+    attack_tree_code = re.sub(r'^```mermaid\s*|\s*```$', '', attack_tree_code, flags=re.MULTILINE)
+
+    return attack_tree_code
---
+++ mitigations.py
@@ -0,0 +1,127 @@
+import requests
+from mistralai import Mistral
+from openai import OpenAI, AzureOpenAI
+
+import google.generativeai as genai
+
+# Function to create a prompt to generate mitigating controls
+def create_mitigations_prompt(threats):
+    prompt = f"""
+Act as a cyber security expert with more than 20 years experience of using the STRIDE threat modelling methodology. Your task is to provide potential mitigations for the threats identified in the threat model. It is very important that your responses are tailored to reflect the details of the threats.
+
+Your output should be in the form of a markdown table with the following columns:
+    - Column A: Threat Type
+    - Column B: Scenario
+    - Column C: Suggested Mitigation(s)
+
+Below is the list of identified threats:
+{threats}
+
+YOUR RESPONSE (do not wrap in a code block):
+"""
+    return prompt
+
+
+# Function to get mitigations from the GPT response.
+def get_mitigations(api_key, model_name, prompt):
+    client = OpenAI(api_key=api_key)
+
+    response = client.chat.completions.create(
+        model = model_name,
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant that provides threat mitigation strategies in Markdown format."},
+            {"role": "user", "content": prompt}
+        ]
+    )
+
+    # Access the content directly as the response will be in text format
+    mitigations = response.choices[0].message.content
+
+    return mitigations
+
+
+# Function to get mitigations from the Azure OpenAI response.
+def get_mitigations_azure(azure_api_endpoint, azure_api_key, azure_api_version, azure_deployment_name, prompt):
+    client = AzureOpenAI(
+        azure_endpoint = azure_api_endpoint,
+        api_key = azure_api_key,
+        api_version = azure_api_version,
+    )
+
+    response = client.chat.completions.create(
+        model = azure_deployment_name,
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant that provides threat mitigation strategies in Markdown format."},
+            {"role": "user", "content": prompt}
+        ]
+    )
+
+    # Access the content directly as the response will be in text format
+    mitigations = response.choices[0].message.content
+
+    return mitigations
+
+# Function to get mitigations from the Google model's response.
+def get_mitigations_google(google_api_key, google_model, prompt):
+    genai.configure(api_key=google_api_key)
+    model = genai.GenerativeModel(
+        google_model,
+        system_instruction="You are a helpful assistant that provides threat mitigation strategies in Markdown format.",
+    )
+    response = model.generate_content(prompt)
+    try:
+        # Extract the text content from the 'candidates' attribute
+        mitigations = response.candidates[0].content.parts[0].text
+        # Replace '\n' with actual newline characters
+        mitigations = mitigations.replace('\\n', '\n')
+    except (IndexError, AttributeError) as e:
+        print(f"Error accessing response content: {str(e)}")
+        print("Raw response:")
+        print(response)
+        return None
+
+    return mitigations
+
+# Function to get mitigations from the Mistral model's response.
+def get_mitigations_mistral(mistral_api_key, mistral_model, prompt):
+    client = Mistral(api_key=mistral_api_key)
+
+    response = client.chat.complete(
+        model = mistral_model,
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant that provides threat mitigation strategies in Markdown format."},
+            {"role": "user", "content": prompt}
+        ]
+    )
+
+    # Access the content directly as the response will be in text format
+    mitigations = response.choices[0].message.content
+
+    return mitigations
+
+# Function to get mitigations from Ollama hosted LLM.
+def get_mitigations_ollama(ollama_model, prompt):
+    
+    url = "http://localhost:11434/api/chat"
+
+    data = {
+        "model": ollama_model,
+        "stream": False,
+        "messages": [
+            {
+                "role": "system", 
+                "content": "You are a helpful assistant that provides threat mitigation strategies in Markdown format."},
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    }
+    response = requests.post(url, json=data)
+
+    outer_json = response.json()
+    
+    # Access the 'content' attribute of the 'message' dictionary
+    mitigations = outer_json["message"]["content"]
+
+    return mitigations
