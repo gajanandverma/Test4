7335c11f706c88d682fb642444c024c4b348f60e
gajanandverma <gajanandverma@gmail.com>
2025-02-19 19:54:34 +0530

    cccc

--- main.py
+++ main.py
@@ -301,16 +301,10 @@ with st.sidebar:
         )
 
     if model_provider == "Mistral API":
-        st.markdown(
-        """
-
-          <style>
-     
-        .stApp {
+        st.markdown(       
             
-            color: #FFDD44;  /* black Text */
-        }      
-    </style>
+            """        
+   
     1. Enter your [Mistral API key](https://console.mistral.ai/api-keys/) and chosen model below 🔑
     2. Provide details of the application that you would like to threat model  📝
     3. Generate a threat list, attack tree and/or mitigating controls for your application 🚀
