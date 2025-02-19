69abbc6e3a3f0e3e05be7416812c7d1044821292
gajanandverma <gajanandverma@gmail.com>
2025-02-19 19:05:14 +0530

    ccc

--- main.py
+++ main.py
@@ -365,7 +365,7 @@ with st.sidebar:
     st.markdown(
          "Threat modelling is a key activity in the software development lifecycle, but is often overlooked or poorly executed. TARA aims to help teams produce more comprehensive threat models by leveraging the power of Large Language Models (LLMs) to generate a threat list, attack tree and/or mitigating controls for an application based on the details provided."
     )
-    # st.markdown("Created by [Matt Adams](https://www.linkedin.com/in/matthewrwadams/).")
+ 
     # Add "Star on GitHub" link to the sidebar
     st.sidebar.markdown(
        "⭐ Star on GitHub: [![Star on GitHub](https://img.shields.io/github/stars/mrwadams/stride-gpt?style=social)](https://github.com/mrwadams/stride-gpt)"
