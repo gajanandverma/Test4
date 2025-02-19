18532d5bb32b862df81cdecee3750f20b5209573
gajanandverma <gajanandverma@gmail.com>
2025-02-19 19:29:50 +0530

    ccc

--- main.py
+++ main.py
@@ -450,8 +450,8 @@ st.markdown(
 
         /* Heading Colors */
         h1, h2, h3 {
-            /* color: #FFDD44;   Gold */
-            color: #FFFFFF;  /* White Text */
+            color: #FFDD44;  /* Gold */
+            
         }
 
         /* Sidebar Background */
