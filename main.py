0903e95b39ceb99132bfee1f7825e54c935ad4cc
gajanandverma <gajanandverma@gmail.com>
2025-02-23 15:55:52 +0530

    cc

--- main.py
+++ main.py
@@ -209,16 +209,21 @@ with st.sidebar:
     )
 
     if model_provider == "OpenAI API":
-        st.markdown(
-        """
-
+       st.markdown(
+    """
+    <style>
+    /* Targets the marker of ordered list items and sets its color to black */
+    ol > li::marker {
+        color: black;
+    }
+    </style>
     
-    1. Enter your [OpenAI API key](https://platform.openai.com/account/api-keys) and chosen model below 🔑
-    2. Provide details of the application that you would like to threat model  📝
+    1. Enter your [OpenAI API key](https://platform.openai.com/account/api-keys) and chosen model below 🔑  
+    2. Provide details of the application that you would like to threat model 📝  
     3. Generate a threat list, attack tree and/or mitigating controls for your application 🚀
-
-    """ 
-    )
+    """,
+    unsafe_allow_html=True
+)
         # Add OpenAI API key input field to the sidebar
         openai_api_key = st.text_input(
             "Enter your OpenAI API key:",
